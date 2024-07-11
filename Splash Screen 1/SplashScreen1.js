9b26f4c7c49edfb6d00180a5c72970244d17322a
Anurag Singh <anurag.dhanju@gmail.com>
2024-07-11 17:03:42 -0400

    Create SplashScreen1.js

---
+++ Splash Screen 1/SplashScreen1.js
@@ -0,0 +1,21 @@
+(function(){
+ 
+ var preload = document.getElementById("preload");
+ var loading = 0;
+ var id = setInterval(frame, 30);
+
+ function frame(){
+  if(loading == 100) {
+   clearInterval(id);
+   window.open("https://www.google.ca/", "_self");
+  }
+  else {
+   loading = loading + 1;
+   if(loading == 90) {
+    preload.style.animation = "fadeout 1s ease";
+   }
+  }
+ }
+
+
+})();
